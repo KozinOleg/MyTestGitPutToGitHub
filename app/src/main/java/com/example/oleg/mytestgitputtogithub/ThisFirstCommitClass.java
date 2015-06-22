c29b26ccbea2cfb3c992bea951d0ce42f4063f1a
Oleg <oleg@neptune.(none)>
2015-06-22 09:56:55 +0300

    second commit

---
+++ app/src/main/java/com/example/oleg/mytestgitputtogithub/ThisFirstCommitClass.java
@@ -0,0 +1,34 @@
+package com.example.oleg.mytestgitputtogithub;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @author Oleg Kozin ok@dalivsoft.com
+ * @since 22.06.15
+ */
+public class ThisFirstCommitClass extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
