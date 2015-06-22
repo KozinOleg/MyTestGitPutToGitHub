658715e9408bc34271127b31481010b7e3c40860
Oleg <oleg@neptune.(none)>
2015-06-22 10:01:21 +0300

    This is third commit

---
+++ app/src/main/java/com/example/oleg/mytestgitputtogithub/ThisThirdCommitClass.java
@@ -0,0 +1,17 @@
+package com.example.oleg.mytestgitputtogithub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * @author Oleg Kozin ok@dalivsoft.com
+ * @since 22.06.15
+ */
+public class ThisThirdCommitClass extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
