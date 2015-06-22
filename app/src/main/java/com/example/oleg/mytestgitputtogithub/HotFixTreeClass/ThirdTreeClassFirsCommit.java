b0ff5cfb6e92f335fb652105b09e26b23c685392
Oleg <oleg@neptune.(none)>
2015-06-22 10:51:57 +0300

    TrirdTreeFirstCommit

---
+++ app/src/main/java/com/example/oleg/mytestgitputtogithub/HotFixTreeClass/ThirdTreeClassFirsCommit.java
@@ -0,0 +1,17 @@
+package com.example.oleg.mytestgitputtogithub.HotFixTreeClass;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * @author Oleg Kozin ok@dalivsoft.com
+ * @since 22.06.15
+ */
+public class ThirdTreeClassFirsCommit extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
